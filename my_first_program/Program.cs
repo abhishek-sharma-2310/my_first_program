142e7ef73654583d11563a6a4f2207a78b1f2ccc
Abhishek Sharma <abhishek.sharma@socko.in>
2021-12-24 10:30:58 +0530

    All file changes

---
+++ my_first_program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_program
+{
+    // Calulate addition, subtraction and multiplication of two numbers 30 and 50
+
+    /* check whether a triangle is scalene, isosecles and equilateral.
+     */
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {   
+           int a = 10;
+            for(int i =0;i<10;i++){
+               
+                a=a+10;
+            }
+            
+            Console.WriteLine(a);
+           Console.ReadKey();
+        }
+    }
+
+}
+
+
